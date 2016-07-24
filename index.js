d05a297565a6314ca1e51631474bc46611005e39
Pandoraemon <pz880304@163.com>
2016-07-24 15:04:10 +0800

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
   var result = {
     "IPaddress": req.get("x-forwarded-for"),
     "Language": req.get("accept-language").split(",")[0],
-    "OS": (/\(([^\(])+\)/g).exec(req.get("user-agent"))[1]//.split(";")[1]
+    "OS": (/\(([^\(])+\)/g).exec(req.get("user-agent"))[0]//.split(";")[1]
   };
   res.send(JSON.stringify(result));
 });
